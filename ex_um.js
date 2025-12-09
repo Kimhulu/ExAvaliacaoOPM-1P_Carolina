83387ed438fa3e45849b4082fe704ecce375e32f
Kimhulu <carolpizarroborges@gmail.com>
2025-12-09 09:31:46 +0000

    Criação Ex-2

--- ex_um.js
+++ ex_um.js
@@ -1,21 +1,14 @@
-
 alert("Olá Mundo!");
-
 
 let nome = prompt("Insere o teu nome:");
 
-
 alert("Olá " + nome + "!");
-
 
 let artista = prompt("Indica um dos teus artistas multimédia de eleição:");
 
-
 alert("Não conheço a/o " + artista + "! Eu gosto muito da Mileece: https://www.mileece.is/bio");
 
-
 let continuar = prompt("Esta página é um arquivo de artistas multimédia. Pretendes continuar? Responde 1 caso sim, ou 0 caso não:");
-
 
 if (continuar === "1") {
     alert("Ótimo! Vamos continuar!");
